6445f164398e8619b5b49ae1803a1e2168404099
Purushotam Sharma <107574991+PurushotamSharma@users.noreply.github.com>
2023-02-23 17:00:51 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://gpt-q17d.onrender.com', {
+    const response = await fetch('https://localhost:5000', 
+    {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
